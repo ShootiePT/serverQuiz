1d0f3f4c45fbbdd680e30ccabcf593ca2e315c51
Shootie <jose.f.rocha92@gmail.com>
2024-05-01 22:59:26 +0100

    add main and quiz logic

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,40 @@
+import java.io.IOException;
+import java.net.ServerSocket;
+import java.net.Socket;
+import java.util.LinkedList;
+
 public class Main {
+    private static final LinkedList<PlayerConnection> playersConnections = new LinkedList<>();
+    private static final int MAX_PLAYERS = 3;
+
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+
+        QuizGame game = new QuizGame(playersConnections);
+
+        try {
+            ServerSocket serverSocket = new ServerSocket(9004);
+
+            while (true) {
+                if (playersConnections.size() >= MAX_PLAYERS) {
+                    break;
+                }
+                Socket clientSocket = serverSocket.accept();
+
+                PlayerConnection playerConnection = new PlayerConnection(clientSocket);
+                playersConnections.add(playerConnection);
+
+                Thread clientThread = new Thread(playerConnection);
+                clientThread.start();
+                //System.out.println(playersConnections.get(0).name);
+                if (playersConnections.size() == MAX_PLAYERS) {
+
+                    game.start();
+                    game.endGame();
+
+                }
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 }
---
+++ src/QuizGame.java
@@ -0,0 +1,125 @@
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.List;
+
+public class QuizGame {
+    private LinkedList<PlayerConnection> playerConnections;
+    private QuestionAnswer questionAnswer = new QuestionAnswer();
+    private List<String> questions;
+    private List<String> answers;
+    private int currentQuestionIndex;
+
+    //private final QuestionAnswer questionAnswer;
+
+    public QuizGame(LinkedList<PlayerConnection> playerConnection) {
+        this.playerConnections = playerConnection;
+
+        // this.questionAnswer = questionAnswer;
+    }
+
+    public void sendQuestion() {
+
+        List<Thread> threads = new ArrayList<>();
+
+        for (PlayerConnection player : playerConnections) {
+            Thread t = new Thread(() -> {
+                //System.out.println(Thread.currentThread().getName());
+                questionAnswer.question1(player);
+            });
+            t.start();
+            threads.add(t);
+        }
+
+        for (Thread thread : threads) {
+            try {
+                thread.join();
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+
+
+    public void start() {
+        while (true) {
+
+            boolean allPlayersReady = true;
+
+            for (int i = 0; i < playerConnections.size(); i++) {
+                if (!playerConnections.get(i).isPlayerReady()) {
+                    allPlayersReady = false;
+                }
+            }
+            if (allPlayersReady) {
+                sendQuestion();
+                break;
+            }
+        }
+    }
+
+    public void endGame() throws IOException {
+        while (true) {
+
+            boolean badjoras = false;
+            for (PlayerConnection player : playerConnections) {
+                if (!player.gameEnd) {
+                    player.gameEnd = true;
+                    badjoras = player.gameEnd;
+                }
+            }
+            if (badjoras) {
+                sendScore();
+                break;
+
+            }
+        }
+    }
+
+    public void sendScore() throws IOException {
+        int highestScore = Integer.MIN_VALUE;
+        List<PlayerConnection> winners = new ArrayList<>();
+
+        for (PlayerConnection player : playerConnections) {
+            int score = player.getScore();
+
+            System.out.println(player.name + " got " + score + " points");
+
+            if (score > highestScore) {
+                highestScore = score;
+                winners.clear();
+                winners.add(player);
+            } else if (score == highestScore) {
+                winners.add(player);
+            }
+        }
+
+        if (!winners.isEmpty()) {
+            if (winners.size() == 1) {
+                PlayerConnection winner = winners.get(0);
+                for(PlayerConnection player : playerConnections) {
+                    PrintWriter writer = new PrintWriter(player.getClientSocket().getOutputStream(), true);
+                    writer.println("The winner is " + winner.name + " with a score of " + highestScore);
+                }
+            } else {
+                for(PlayerConnection player : playerConnections) {
+                    PrintWriter writer = new PrintWriter(player.getClientSocket().getOutputStream(), true);
+                    writer.println("There are multiple winners with a score of " + highestScore + ":");
+                }
+                for (PlayerConnection winner : winners) {
+                    for(PlayerConnection player : playerConnections) {
+                        PrintWriter writer = new PrintWriter(player.getClientSocket().getOutputStream(), true);
+                        writer.println(winner.name);
+                    }
+                }
+            }
+        } else {
+            for(PlayerConnection player : playerConnections) {
+                PrintWriter writer = new PrintWriter(player.getClientSocket().getOutputStream(), true);
+                writer.println("No winner found.");
+            }
+        }
+
+    }
+}
